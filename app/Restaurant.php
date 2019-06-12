d473815bb088ff0b82b19cda143ff0095aa3eaa6
cptHastings <leelabanne@gmail.com>
2019-06-12 14:58:40 +0200

    cleanup

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -2,15 +2,11 @@
 
 use Illuminate\Database\Eloquent\Model;
 
-class Restaurant extends Model {
+class Restaurant extends Model
+{
 
     public $timestamps = false;
     protected $fillable = [];
     protected $dates = [];
-    public static $rules = [
-        // Validation rules
-    ];
-
-    // Relationships
-
+    public static $rules = [];
 }
